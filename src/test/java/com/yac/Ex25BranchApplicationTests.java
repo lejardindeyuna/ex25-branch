1cbcfaed284a3fc38419d5de55de1dc8a8bbead4
lejardindeyuna <emevoluoywonkl@gmail.com>
2025-10-29 12:38:27 +0900

    Initial commit

---
+++ src/test/java/com/yac/Ex25BranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex25BranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
